72f3a9fb12932d730167ea21f42aaacff1ed1cc9
Léonard <leo.rivaux@gmail.com>
2024-09-30 13:46:23 +0200

    patate

---
+++ Pendu_lib.py
@@ -0,0 +1,6 @@
+#Jeu du Pendu
+#9/23/24
+#Léonard Rivaux
+#ToDo:
+
+
---
+++ pendu_interface_lib.py
@@ -0,0 +1,22 @@
+#Jeu du Pendu
+#9/23/24
+#Léonard Rivaux
+#ToDo:
+
+import random as rd
+import tkinter as tk
+
+def lire_dico(name):
+    data = open(name).read() 
+
+    l_dico = list(data.split())
+    return l_dico
+
+def lancement_partie():
+    pendu(0)
+
+
+def pendu(meilleurscore):
+    listemots = lire_dico("dico.txt")
+    motrandom = listemots[rd.randint(0,len(listemots))]
+    print(motrandom)
